8cfcef605716755d17658c42ba5e40a64678db45
Vivek Kumar <vivek@Viveks-MacBook-Air.local>
2025-05-10 15:02:55 +0530

    add test logic

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ def sub(a, b):
     return a-b
 
 
-result1 = add(4, 5)
-resutl2 = sub(4, 1)
+if __name__ == "__main__":
+    result1 = add(4, 5)
+    resutl2 = sub(4, 1)
 
-print(result1, resutl2)
+    print(result1, resutl2)
